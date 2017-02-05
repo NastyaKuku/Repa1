a6de5f836b85d0f6c6fd0f1d1bd74273d34dc516
NastyaKuku <nastya_rij@mail.ru>
2017-02-05 23:27:54 +0800

    сложение

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+System.out.println("введите 2 числа:");
+int a = sc.nextInt();
+int b = sc.nextInt();
+int sum=a+b;
+System.out.println("сумма="+sum);
+}   
+	}
+
+
