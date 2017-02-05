c2b8ae0c9ae9204e488dd5194127d2ea2d04072f
NastyaKuku <nastya_rij@mail.ru>
2017-02-06 01:04:15 +0800

    issue #1

---
+++ raz.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class raz {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+System.out.println("введите 2 числа:");
+int a = sc.nextInt();
+int b = sc.nextInt();
+int raz=a-b;
+System.out.println("разность="+raz);
+}   
+	}
+
+
