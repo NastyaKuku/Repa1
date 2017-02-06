a14d1d455f4f72b374bac070838c6c6c290e6e17
NastyaKuku <nastya_rij@mail.ru>
2017-02-06 17:44:53 +0800

    умножение issue #2

---
+++ mul.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class mul {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+System.out.println("введите 2 числа:");
+int a = sc.nextInt();
+int b = sc.nextInt();
+int mul=a*2;
+System.out.println("произведение="+mul);
+}   
+	}
+
+
